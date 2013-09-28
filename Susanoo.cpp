64130322c5d6d2f11a453f4eaf8f549a946e5778
Gaston.martinez.90@gmail.com <Gaston.martinez.90@gmail.com@6fcf5558-922e-3354-a0a2-e2fc61f0ea5b>
2013-09-28 21:35:47 +0000

    Agrego las cosas basicas que estaban en el otro repo

---
+++ Susanoo.cpp
@@ -0,0 +1,118 @@
+/**
+Susanoo.cpp
+
+Copyright 2013 Debora Elisa Martin <>
+               Gaston Alberto Martinez <Gaston.martinez.90@gmail.com>
+               Matias Fernando Vazquez <>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses
+*/
+
+#include <stdio.h>
+#include <getopt.h>
+#include <iostream>
+
+#include "vectorizador.h"
+
+using std::cout;
+using std::string;
+
+void print_help()
+{
+    printf("Modo de uso: TpGrupoX [opciones] argumentos \n\n\
+    *Opciones de construccion de grupos: \n\
+        Opciones: --directorio|-d <path> --multi|-o <Y/N> [--categorias|-c] <cantidad>\n\
+        [-d] Indica el path a donde están almacenados los documentos.\n\
+        [-c] Indica la cantidad de categorias a crear, si no se indica se decide automaticamente.\n\
+        [-o] Indica si un documento puede estar en mas de un grupo.\n\n\
+    *Opciones de indexacion: \n\
+        Opciones: []\n\
+        [-l] Lista todos los documentos del repositorio y la categoría a la cual pertenece cada uno.\n\
+        [-g] Lista los grupos o categorías existentes y los documentos dentro de cada grupo o categoría.\n\
+        [-a] Agrega y clasifica el texto pasado como parametro e indica a que grupo lo ha agregado\n\
+             (a partir de aqui debería aparecer al listad con -l o -g)\n\n"
+    );
+}
+
+int main (int argc, char **argv)
+{
+    int c;
+
+    static struct option long_options[] = {
+        {"directorio", 1, 0, 0},
+        {"categorias", 1, 0, 0},
+        {"multi", 1, 0, 0},
+        {"agregar", 1, 0, 0},
+        {"listar", 0, 0, 0},
+        {"grupos", 0, 0, 0},
+        {"help", 0, 0, 0},
+        {NULL, 0, NULL, 0}
+    };
+
+    int option_index = 0;
+
+    while ((c = getopt_long(argc, argv, "d:c:o:a:lgh", long_options, &option_index)) != -1)
+    {
+        if (c == 0){
+            c = long_options[option_index].name[0];
+            }
+
+        switch (c) {
+
+        case 'a':
+            printf ("option %c with value '%s'\n",c,optarg);
+            break;
+
+        case 'c':
+            printf ("option %c with value '%s'\n",c,optarg);
+            break;
+
+        case 'd':
+            printf ("option %c with value '%s'\n",c,optarg);
+            vectorizar(optarg);
+            break;
+
+        case 'g':
+            printf ("option %c\n",c);
+            break;
+
+        case 'l':
+            printf ("option %c\n",c);
+            break;
+
+        case 'm':
+        case 'o':
+            printf ("option %c with value '%s'\n",c,optarg);
+            break;
+
+        case 'h':
+            print_help();
+            break;
+
+        case '?':
+            break;
+
+        default:
+            printf ("?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+    if (optind < argc) {
+        printf ("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf ("%s ", argv[optind++]);
+        printf ("\n");
+    }
+
+    return 0;
+}
---
+++ vectorizador.cpp
@@ -0,0 +1,164 @@
+/**
+vectorizador.cpp
+
+Copyright 2013 Debora Elisa Martin <>
+               Gaston Alberto Martinez <Gaston.martinez.90@gmail.com>
+               Matias Fernando Vazquez <>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses
+*/
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <map>          // std::map
+#include <vector>       // std::vector
+#include <string>       // std::string
+#include <iostream>     // std::cout|cerr|endl
+#include <fstream>      // std::ifstream|ofstream
+
+#include <algorithm>    // std::sort
+
+#include "vectorizador.h"
+
+using std::string;
+using std::vector;
+using std::map;
+
+using std::sort;
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+using std::ifstream;
+using std::ofstream;
+
+void a_minuscula(string& cadena)
+{
+    transform(cadena.begin(), cadena.end(), cadena.begin(), ::tolower);
+}
+
+bool es_caracter(char caracter)
+{
+    return (('A'<=caracter and caracter<='Z') or
+            ('a'<=caracter and caracter<='z') );
+}
+//map<string,int> palabras_prohibidas
+
+void imprimir_archivos(vector<string>& archivos)
+{
+    for (unsigned int i = 0;i < archivos.size();i++) {
+        cout << archivos[i] << endl;
+    }
+}
+
+void contar (string archivo,map<string,vector<int> >& palabras_archivos,int i)
+{
+     ifstream arch;
+     arch.open(archivo.c_str());
+     map<string,int> palabras = map<string,int>();
+     while (arch.good())
+     {
+        char c = arch.get();
+        string res = string("");
+
+        while (arch.good() and es_caracter(c))
+        {
+            res += c;
+            c = arch.get();
+        }
+
+        if (res.size()<3) continue;//funcion es palabra valida
+                                  //que valida eso y saca palabras
+                                 //chotas tipo if in on out at etc.
+
+        a_minuscula(res);
+
+        int cant = palabras.count(res);
+        if (cant == 0)
+        {
+            palabras[res] = 1;
+            continue;
+        }
+        palabras[res] = palabras[res] + 1;
+
+        cant = palabras_archivos.count(res);
+        if (cant == 0)
+        {
+            palabras_archivos[res] = vector<int>();
+            palabras_archivos[res].push_back(1);
+            palabras_archivos[res].push_back(i);
+            continue;
+        }
+        else
+        {
+            if (palabras_archivos[res][1]>=i) continue;
+            palabras_archivos[res][0] = palabras_archivos[res][0] + 1;
+            palabras_archivos[res][1] = i;
+        }
+     }
+
+     ofstream out;
+     out.open((archivo+".res").c_str());
+     for (map<string,int>::iterator it=palabras.begin();it!=palabras.end();++it)
+     {
+        out << it->first << "=" << it->second << endl;
+     }
+     out.close();
+}
+
+void generar_bases(vector<string>& archivos)
+{
+    map<string, vector<int> > palabras_archivos = map<string,vector<int> >();
+
+    for (unsigned int i = 0;i < archivos.size();i++) {
+        contar(archivos[i],palabras_archivos,i);
+    }
+
+     ofstream out;
+     out.open("./Total.res");
+     for (map<string,vector<int> >::iterator it=palabras_archivos.begin();it!=palabras_archivos.end();++it)
+     {
+        if (it->second[0] < 0.1* archivos.size()) continue;
+        out << it->first << "=" << it->second[0] << endl;
+     }
+     out.close();
+}
+
+void obtener_archivos(string directorio,vector<string>& archivos)
+{
+    DIR *dp;
+    struct dirent *dirp;
+
+    if((dp  = opendir(directorio.c_str())) == NULL) {
+        cerr << "Error abriendo " << directorio << endl;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        archivos.push_back(string(directorio+"/"+dirp->d_name));
+    }
+    closedir(dp);
+}
+
+void vectorizar (string directorio)
+{
+    vector<string> archivos = vector<string>();
+    map<string,int> palabras_archivos = map<string,int>();
+
+    obtener_archivos(directorio,archivos);
+
+    sort(archivos.begin(),archivos.end());
+
+    generar_bases(archivos);
+}
