5b768192da4dd004c531a85f008b0a6b4b808cff
Xero-Hige <gaston.martinez.90@gmail.com>
2014-08-22 03:54:58 -0300

    Limpie algunas boludeces del centroide

--- centroide.cpp
+++ centroide.cpp
@@ -20,7 +20,6 @@
 #include "centroide.h"
 
 #include <stddef.h>
-//#include <time.h>
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
@@ -56,7 +55,8 @@ Centroide::Centroide(int dimensiones, bool random) {
 
 	modulo = sqrt(modulo);
 
-	vectores_asociados = random ? 1 : 0;
+	//Si no es random el centroide no tiene vectores asociados
+	vectores_asociados = random ? 1 : 0; 
 
 	double suma_acumulados_cuadrado = 0;
 
@@ -69,8 +69,6 @@ Centroide::Centroide(int dimensiones, bool random) {
 	} else {
 		acumulados = coordenadas;
 	}
-//
-//	modulo = sqrt(suma_acumulados_cuadrado);
 }
 
 double Centroide::calcular_coseno(map<int, double>& vector_reducido) {
@@ -85,8 +83,6 @@ double Centroide::calcular_coseno(map<int, double>& vector_reducido) {
 		modulo_vector += valor * valor;
 	}
     modulo_vector = sqrt(modulo_vector);
-
-    //std::cout << "Coseno V: " << resultado / (modulo * modulo_vector) << std::endl;
 
 	return (resultado / (modulo * modulo_vector));
 }
@@ -104,22 +100,17 @@ double Centroide::calcular_coseno(Centroide& otro_centroide) {
 	double mod_a = modulo;
 	double mod_b = otro_centroide.modulo;
 
-//	std::cout << "Coseno C: " <<(resultado / (mod_a * mod_b)) << std::endl;
-
 	return (resultado / (mod_a * mod_b));
 }
 
 void Centroide::agregar_vector(map<int, double>& vector_reducido) {
-//	std::cout << "init" << std::endl;
-//  suma_acumulados_cuadrado -= (anterior * anterior);
-//	double suma_acumulados_cuadrado = 0; //+= (actual * actual);
 
 	for (map<int, double>::iterator it = vector_reducido.begin();
 			it != vector_reducido.end(); ++it) {
 
 		int coordenada = it->first;
 		double valor = it->second;
-//    std::cout << "coordenada = " << coordenada << "                       valor = " << valor << std::endl;
+
 		acumulados[coordenada] += valor;
 	}
 	vectores_asociados++;
@@ -170,9 +161,6 @@ Centroide::Centroide(const std::string& archivo){
   }
   arch_centroide.close();
 }
-
-
-
 
 void Centroide::guardar(const string& ruta_carp_centroide, int nro_centroide) {
     normalizar();
